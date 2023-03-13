6c6b3ff39a9c745a1cf31c016f66d740dde25b6e
jcaldicote <caldicote.j@gmail.com>
2023-03-13 03:51:01 +0100

    All Works display OK

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,14 +1,23 @@
-// requete de recuperation des travaux via Fetch
+// requête de recuperation des travaux via Fetch
 
 const reponse = await fetch('http://localhost:5678/api/works');
 const travaux = await reponse.json();
 
-const galleryTravaux = travaux[0];
-
-const portfolioElement = document.getElementById('portfolio');
-
+const enteteTravaux = document.createElement("h2");
+enteteTravaux.innerText = "Récupération des travaux";
 const galleryElement = document.createElement("div");
 galleryElement.classList.add('gallery');
+
+
+//Boucle for pour prendre compte tt les img des travaux
+for (let i = 0; i < travaux.length; i++) {
+const galleryTravaux = travaux[i];
+
+//récupération de l'élément 
+const portfolioElement = document.getElementById('portfolio');
+
+
+//Création des balises
 
 const cadreImageTravaux = document.createElement("figure");
 const imgTravaux = document.createElement("img");
@@ -16,9 +25,9 @@ imgTravaux.src = galleryTravaux.imageUrl;
 const titreTravaux = document.createElement("figcaption");
 titreTravaux.innerText = galleryTravaux.title;
 
-
-
-
+//Attachement des balises au DOM
+portfolioElement.appendChild(enteteTravaux);
 portfolioElement.appendChild(galleryElement).appendChild(cadreImageTravaux);
 cadreImageTravaux.appendChild(imgTravaux);
 cadreImageTravaux.appendChild(titreTravaux);
+};
