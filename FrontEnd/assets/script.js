570cd1d83e8e72297fd78236605bd8c9736cdf95
jcaldicote <caldicote.j@gmail.com>
2023-03-13 21:37:36 +0100

    move js balise

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -4,31 +4,29 @@ const reponse = await fetch('http://localhost:5678/api/works');
 const travaux = await reponse.json();
 
 //Création H2 + DIV + class
-const enteteTravaux = document.createElement("h2");
-enteteTravaux.innerText = "Récupération des travaux";
-const galleryElement = document.createElement("div");
+const enteteTravaux = document.createElement('h2');
+enteteTravaux.innerText = 'Récupération des travaux';
+const galleryElement = document.createElement('div');
 galleryElement.classList.add('gallery');
-
 
 //Boucle for pour prendre compte tt les img des travaux
 for (let i = 0; i < travaux.length; i++) {
-const galleryTravaux = travaux[i];
+  const galleryTravaux = travaux[i];
 
-//récupération de l'élément 
-const portfolioElement = document.getElementById('portfolio');
+  //récupération de l'élément
+  const portfolioElement = document.getElementById('portfolio');
 
+  //Création des balises
 
-//Création des balises
+  const cadreImageTravaux = document.createElement('figure');
+  const imgTravaux = document.createElement('img');
+  imgTravaux.src = galleryTravaux.imageUrl;
+  const titreTravaux = document.createElement('figcaption');
+  titreTravaux.innerText = galleryTravaux.title;
 
-const cadreImageTravaux = document.createElement("figure");
-const imgTravaux = document.createElement("img");
-imgTravaux.src = galleryTravaux.imageUrl;
-const titreTravaux = document.createElement("figcaption");
-titreTravaux.innerText = galleryTravaux.title;
-
-//Attachement des balises au DOM
-portfolioElement.appendChild(enteteTravaux);
-portfolioElement.appendChild(galleryElement).appendChild(cadreImageTravaux);
-cadreImageTravaux.appendChild(imgTravaux);
-cadreImageTravaux.appendChild(titreTravaux);
-};
+  //Attachement des balises au DOM
+  portfolioElement.appendChild(enteteTravaux);
+  portfolioElement.appendChild(galleryElement).appendChild(cadreImageTravaux);
+  cadreImageTravaux.appendChild(imgTravaux);
+  cadreImageTravaux.appendChild(titreTravaux);
+}
