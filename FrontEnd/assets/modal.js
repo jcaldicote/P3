c9cf48bcb9a40ef88680f4ed727db3a406207050
jcaldicote <caldicote.j@gmail.com>
2023-04-03 06:50:51 +0200

    first modale render ok without trash icon

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,5 +1,11 @@
+import { fetchWorks } from "./api.js";
+import { createAppend } from "./dom.js";
+
 const modalButtons = document.querySelector("[data-toggle = modal");
 const modalModifyButton = document.querySelector(".isAdminModal");
+const modalBody = document.querySelector(".modal-body");
+
+//affichage de la modale
 
 modalButtons.addEventListener("click", function (e) {
   e.preventDefault();
@@ -11,6 +17,28 @@ modalButtons.addEventListener("click", function (e) {
   modal.classList.add("show");
   modalModifyButton.style.display = "block";
 
+  //affichage de la modale avec la galerie par défaut
+
+  function renderWorkModal(work) {
+    const workElm = createAppend("figure", modalBody);
+    const imgElm = createAppend("img", workElm);
+    imgElm.src = work.imageUrl;
+
+    const titreElm = createAppend("figcaption", workElm);
+    titreElm.innerText = "éditer";
+  }
+
+  function renderWorksModal(works) {
+    for (const work of works) renderWorkModal(work);
+  }
+
+  async function renderWorksModalAll() {
+    const travaux = await fetchWorks();
+
+    renderWorksModal(travaux);
+  }
+  renderWorksModalAll();
+
   //pour fermer la modale
 
   const modalClose = modal.querySelector("[data-dismiss=dialog]");
@@ -18,11 +46,13 @@ modalButtons.addEventListener("click", function (e) {
   modalClose.addEventListener("click", () => {
     modal.classList.remove("show");
     modalModifyButton.style.display = "none";
+    modalBody.innerHTML = "";
   });
 
   modal.addEventListener("click", function () {
     this.classList.remove("show");
     modalModifyButton.style.display = "none";
+    modalBody.innerHTML = "";
   });
   modal.children[0].addEventListener("click", function (e) {
     e.stopPropagation();
