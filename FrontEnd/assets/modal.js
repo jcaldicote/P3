24e863ddb09b1cba93c3e75b063331a5ca260d8a
jcaldicote <caldicote.j@gmail.com>
2023-04-03 05:21:15 +0200

    first modal modal.js correction

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,32 +1,30 @@
-const modalButtons = document.querySelectorAll("[data-toggle = modal");
+const modalButtons = document.querySelector("[data-toggle = modal");
 const modalModifyButton = document.querySelector(".isAdminModal");
 
-for (let button of modalButtons) {
-  button.addEventListener("click", function (e) {
-    e.preventDefault();
+modalButtons.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    let target = this.dataset.target;
+  let target = this.dataset.target;
 
-    let modal = document.querySelector(target);
+  let modal = document.querySelector(target);
 
-    modal.classList.add("show");
-    modalModifyButton.style.display = "block";
+  modal.classList.add("show");
+  modalModifyButton.style.display = "block";
 
-    //pour fermer la modale
+  //pour fermer la modale
 
-    const modalClose = modal.querySelectorAll("[data-dismiss=dialog]");
-    for (let close of modalClose) {
-      close.addEventListener("click", () => {
-        modal.classList.remove("show");
-        modalModifyButton.style.display = "none";
-      });
-    }
-    modal.addEventListener("click", function () {
-      this.classList.remove("show");
-      modalModifyButton.style.display = "none";
-    });
-    modal.children[0].addEventListener("click", function (e) {
-      e.stopPropagation();
-    });
+  const modalClose = modal.querySelector("[data-dismiss=dialog]");
+
+  modalClose.addEventListener("click", () => {
+    modal.classList.remove("show");
+    modalModifyButton.style.display = "none";
   });
-}
+
+  modal.addEventListener("click", function () {
+    this.classList.remove("show");
+    modalModifyButton.style.display = "none";
+  });
+  modal.children[0].addEventListener("click", function (e) {
+    e.stopPropagation();
+  });
+});
