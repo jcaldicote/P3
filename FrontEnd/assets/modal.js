36e6389a241b78d80c5f220584389983e24cccf8
jcaldicote <caldicote.j@gmail.com>
2023-04-04 11:40:00 +0200

    add coment under modal.js

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -4,6 +4,32 @@ import { createAppend } from "./dom.js";
 const modalButtons = document.querySelector("[data-toggle = modal");
 const modalModifyButton = document.querySelector(".isAdminModal");
 const modalBody = document.querySelector(".modal-body");
+const modalClose = modal.querySelector("[data-dismiss=dialog]");
+
+//affichage de la galerie par défaut dans la modale non affiché
+
+function renderWorkModal(work) {
+  const workElm = createAppend("figure", modalBody);
+  const imgElm = createAppend("img", workElm);
+  const imgElm2 = createAppend("i", workElm);
+  imgElm.src = work.imageUrl;
+  imgElm2.classList.add("fa-regular");
+  imgElm2.classList.add("fa-trash-can");
+
+  const titreElm = createAppend("figcaption", workElm);
+  titreElm.innerText = "éditer";
+}
+
+function renderWorksModal(works) {
+  for (const work of works) renderWorkModal(work);
+}
+
+async function renderWorksModalAll() {
+  const travaux = await fetchWorks();
+
+  renderWorksModal(travaux);
+}
+renderWorksModalAll();
 
 //affichage de la modale
 
@@ -17,45 +43,18 @@ modalButtons.addEventListener("click", function (e) {
   modal.classList.add("show");
   modalModifyButton.style.display = "block";
 
-  //affichage de la modale avec la galerie par défaut
-
-  function renderWorkModal(work) {
-    const workElm = createAppend("figure", modalBody);
-    const imgElm = createAppend("img", workElm);
-    const imgElm2 = createAppend("i", workElm);
-    imgElm.src = work.imageUrl;
-    imgElm2.classList.add("fa-regular");
-    imgElm2.classList.add("fa-trash-can");
-
-    const titreElm = createAppend("figcaption", workElm);
-    titreElm.innerText = "éditer";
-  }
-
-  function renderWorksModal(works) {
-    for (const work of works) renderWorkModal(work);
-  }
-
-  async function renderWorksModalAll() {
-    const travaux = await fetchWorks();
-
-    renderWorksModal(travaux);
-  }
-  renderWorksModalAll();
-
   //pour fermer la modale
-
-  const modalClose = modal.querySelector("[data-dismiss=dialog]");
 
   modalClose.addEventListener("click", () => {
     modal.classList.remove("show");
     modalModifyButton.style.display = "none";
-    modalBody.innerHTML = "";
+    // modalBody.innerHTML = "";
   });
 
   modal.addEventListener("click", function () {
     this.classList.remove("show");
     modalModifyButton.style.display = "none";
-    modalBody.innerHTML = "";
+    // modalBody.innerHTML = "";
   });
   modal.children[0].addEventListener("click", function (e) {
     e.stopPropagation();
